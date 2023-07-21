dd12eda3336fb6f6a73c1ef6f493e110ae62d0c2
João Paulo Medeiros Rocha <joaopaulo.rocha@aridesa.com.br>
2023-07-21 13:56:54 -0300

    Configurando o sistema de menu responsivo

---
+++ code/script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("body");
+
+document.querySelector(".logoResp").onclick = () => {
+    body.classList.toggle("menu");
+}
+
+document.querySelector(".menuArea .logoResp").onclick = () => {
+    body.classList.toggle("menu");
+}
