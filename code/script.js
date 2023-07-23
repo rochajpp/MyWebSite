6940d9f2e65729822888a051edf52f57cd589bc6
João <joao.rochap03@gmail.com>
2023-07-23 00:08:56 -0300

    Função de efeito de digitação de elemento html feito, estilização a mais acressentada em main.css e definindo, display none para menuArea em tela de computadores, no media.css

--- code/script.js
+++ code/script.js
@@ -4,6 +4,19 @@ document.querySelector(".logoResp").onclick = () => {
     body.classList.toggle("menu");
 }
 
-document.querySelector(".menuArea .logoResp").onclick = () => {
-    body.classList.toggle("menu");
+
+function Writer(element){
+
+    const arrayList = element.innerHTML.split("");
+
+    element.innerHTML = "";
+
+    setTimeout(() => {  
+        arrayList.forEach((letters, index) => {
+            setTimeout(() => {
+                element.innerHTML += letters;
+            }, 50 * index)
+        });
+    
+    }, 2000);
 }
