026e93aacb1db683e0a9d44c0917544d0eef29bc
João Paulo <joao.rochap03@gmail.com>
2024-09-11 12:26:47 -0300

    Adicionando animação no label ao clicar no input

--- code/script.js
+++ code/script.js
@@ -11,4 +11,26 @@ function activeMenu(){
     body.classList.toggle("menu-on");
 }
 
+const label = document.querySelectorAll(".info label");
+const input = document.querySelectorAll(".info input");
+
+input[0].addEventListener('focus', () => {
+    label[0].style.bottom = "2rem";
+})
+
+input[0].addEventListener('blur', function() {
+    if(input[0].value == ""){
+        label[0].style.bottom = "";
+    }
+});
+
+input[1].addEventListener('focus', () => {
+    label[1].style.bottom = "2rem";
+})
+
+input[1].addEventListener('blur', function() {
+    if(input[1].value == ""){
+        label[1].style.bottom = "";
+    }
+});
 
